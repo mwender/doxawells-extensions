74a11646f7c4fd748914a543b3fdadf687795bb0
Michael Wender <michael@michaelwender.com>
2021-12-13 21:10:59 +0000

    First commit

---
+++ doxawells-extensions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     DOXA Wells Extensions
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Various extensions and enhancements to the DOXA Wells website code.
+ * Author:          TheWebist
+ * Author URI:      https://mwender.com
+ * Text Domain:     doxawells-extensions
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Doxawells_Extensions
+ */
+
+// Your code starts here.
